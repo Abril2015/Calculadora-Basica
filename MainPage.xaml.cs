180a6c650698c1b604732b08d71f07d7e724d036
Abril Zayas <72897553+Abril2015@users.noreply.github.com>
2021-01-22 21:02:07 -0700

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace CalculadoraBasica
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            pkOper.Items.Add("Suma");
+            pkOper.Items.Add("Resta");
+            pkOper.Items.Add("Multiplicacion");
+            pkOper.Items.Add("Division");
+
+        }
+
+        private void pkOper_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            String op = pkOper.SelectedItem.ToString();
+            int d1 = Int32.Parse(txtD1.Text);
+            int d2 = Int32.Parse(txtD2.Text);
+            double res = 0;
+
+            if (op == "Suma")
+                res = d1 + d2;
+            else if (op == "Resta")
+                res = d1 - d2;
+            else if (op == "Multiplicacion")
+                res = d1 * d2;
+            else if (op == "Division")
+                res = d1 / d2;
+
+            lblRes.Text = res + "";
+        }
+    }
+}
